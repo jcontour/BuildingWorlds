6f653ca815fcd4c693b2822c733f919f563b7f24
jcontour <contj186@newschool.edu>
2018-10-26 14:59:34 -0400

    teleportation and scene loading

---
+++ Teleportation/Assets/CameraChangeButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChangeButton : MonoBehaviour {
+
+	public GameObject startCam;
+	public GameObject fps;
+
+	public void changeCamera(){
+		startCam.GetComponent<Camera> ().depth = -1;
+		fps.SetActive (true);
+	}
+
+}
---
+++ Teleportation/Assets/Teleportation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleportation : MonoBehaviour {
+
+	public GameObject redCube, blueCube;
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "toRed") {
+			transform.position = blueCube.transform.position + new Vector3 (0, 0, -5);
+		}
+		if (other.gameObject.tag == "toBlue") {
+			transform.position = redCube.transform.position + new Vector3 (0, 0, 5);
+		}
+
+	}
+
+}
