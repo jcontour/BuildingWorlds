9a5cf938791f4744636bdd11ee468004b402690a
jcontour <contj186@newschool.edu>
2018-11-16 14:50:41 -0500

    in class lessons

---
+++ Cameras_UI/Assets/C_Switcher.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C_Switcher : MonoBehaviour {
+
+	public Camera camera1, camera2;
+	public bool mainCameraShowing;
+
+	// Use this for initialization
+	void Start () {
+		mainCameraShowing = true;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (mainCameraShowing) {
+//			camera1.depth = 1;
+			camera1.enabled = true;
+			camera2.enabled = false;
+		} else {
+//			camera1.depth = -1;
+			camera1.enabled = false;
+			camera2.enabled = true;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			mainCameraShowing = !mainCameraShowing;
+		}
+		
+	}
+
+}
---
+++ Cameras_UI/Assets/GUIScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIScript : MonoBehaviour {
+
+	public Text uitext;
+	public Image box1, box2, box3;
+	bool haveBox1, haveBox2, haveBox3;
+
+	public Slider slidey;
+	float timerVal;
+
+	// Use this for initialization
+	void Start () {
+		haveBox1 = false;
+		haveBox2 = false;
+		haveBox3 = false;
+
+		timerVal = 0;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+//		slidey.value = Time.time;
+		if (Input.GetKeyDown(KeyCode.UpArrow)){
+			timerVal++;
+			slidey.value = timerVal;
+		}
+
+		if (haveBox1) {
+			box1.enabled = true;
+		} else {
+			box1.enabled = false;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			haveBox1 = !haveBox1;
+		}
+
+		if (Input.GetKeyDown (KeyCode.W)) {
+			uitext.text = "WOW!!";
+		}
+		if (Input.GetKeyDown (KeyCode.N)) {
+			uitext.text = "NEATO";
+		}
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			uitext.text = "";
+		}
+	}
+}
